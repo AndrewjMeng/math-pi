7bb272bdb5c7c114b05bccf94e41a22e8ed92ce6
Andrew Meng <Andrew.Meng@usaa.com>
2019-02-12 12:02:16 -0600

    initial commit

---
+++ src/main/java/com/example/pie/MathController.java
@@ -0,0 +1,14 @@
+package com.example.pie;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MathController {
+
+    @GetMapping("/math/pi")
+    public String helloWorld() {
+        return "3.141592653589793";
+    }
+
+}
---
+++ src/main/java/com/example/pie/PieApplication.java
@@ -0,0 +1,14 @@
+package com.example.pie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PieApplication.class, args);
+	}
+
+}
+
